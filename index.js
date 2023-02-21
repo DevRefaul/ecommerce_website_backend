287e6b5820102865f488a8a7453d7cfe6039e647
DevRefaul <devrefaul@gmail.com>
2023-02-21 13:52:22 +0600

    added cors and another basic api

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
+
+
+app.use(cors())
+
 
 app.get("/", (req, res) => {
     res.send("Server Is On Air")
 })
 
-app.listen(3000, () => {
-    console.log(`Express is running on posrt ${3000}`);
+app.get("/products", (req, res) => {
+    res.send("All The products are here")
+})
+
+
+
+app.listen(5000, () => {
+    console.log(`Express is running on port ${5000}`);
 })
