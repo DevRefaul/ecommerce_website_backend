e5ec5b688a201c12b102ee6ab7418acc3293d780
DevRefaul <devrefaul@gmail.com>
2023-02-21 14:20:36 +0600

    added fashion data and created api for that

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const app = express()
 const allproducts = require("./Products.json")
 const electronics = require("./electronics.json")
+const fashion = require("./fashion.json")
 
 
 app.use(cors())
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 
 app.get("/allProducts", (req, res) => {
     res.send(allproducts)
+})
+app.get("/fashion", (req, res) => {
+    res.send(fashion)
 })
 app.get("/electronics", (req, res) => {
     res.send(electronics)
