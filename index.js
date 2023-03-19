8073767a81ae40c30f96157e3467709bc5babb39
DevRefaul <devrefaul@gmail.com>
2023-03-19 20:38:34 +0600

    minor change

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const cors = require("cors")
-const app = express()
 require("dotenv").config()
 const {mongoClient} = require("mongodb")
+const app = express()
 
 const uri = `mongodb + srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2z0ouy.mongodb.net/?retryWrites=true&w=majority`
 
