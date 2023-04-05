8bb15cef79cae811bb7338165a6c027f77c2a1b8
DevRefaul <devrefaul@gmail.com>
2023-04-05 15:31:53 +0600

    added function for calculating total amount to pay

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ const dbActions = async () => {
     const Orders = client.db("Rafees_Shop").collection("Orders")
     const Cart = client.db("Rafees_Shop").collection("Cart")
 
+
+
+    // function for calculating total amount to pay
+    const calculateOrderAmount = (items) => {
+        // Replace this constant with a calculation of the order's amount
+        // Calculate the order total on the server to prevent
+        // people from directly manipulating the amount on the client
+        return 1400;
+    };
+
     try {
 
         // basic api
