679176ee39f53d82b6a4e9445786160d655f4898
DevRefaul <devrefaul@gmail.com>
2023-04-05 15:33:41 +0600

    added api for payment intent

--- index.js
+++ index.js
@@ -427,6 +427,26 @@ const dbActions = async () => {
         })
 
 
+        // api for payment
+        app.post("/create-payment-intent", async (req, res) => {
+            const { items } = req.body;
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: calculateOrderAmount(items),
+                currency: "usd",
+                automatic_payment_methods: {
+                    enabled: true,
+                },
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+
+
 
     } catch (error) {
         console.log(error.message);
