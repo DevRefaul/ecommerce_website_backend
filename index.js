1bf63c6a52b3d299b7e73c92a15915b3ea9c649a
DevRefaul <devrefaul@gmail.com>
2023-04-05 16:07:48 +0600

    getting the data from from api to calculate toatl amount to pay

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ const dbActions = async () => {
 
     // function for calculating total amount to pay
     const calculateOrderAmount = (items) => {
-        // Replace this constant with a calculation of the order's amount
-        // Calculate the order total on the server to prevent
-        // people from directly manipulating the amount on the client
+
         console.log(items);
     };
 
@@ -454,16 +452,17 @@ const dbActions = async () => {
 
         // api for payment
         app.post("/create-payment-intent", async (req, res) => {
-            const { items } = req.body;
+            const items = req.body;
 
+            calculateOrderAmount(items);
             // Create a PaymentIntent with the order amount and currency
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount: calculateOrderAmount(items),
-                currency: "usd",
-                automatic_payment_methods: {
-                    enabled: true,
-                },
-            });
+            // const paymentIntent = await stripe.paymentIntents.create({
+            //     amount: calculateOrderAmount(items),
+            //     currency: "usd",
+            //     automatic_payment_methods: {
+            //         enabled: true,
+            //     },
+            // });
 
             // res.send({
             //     clientSecret: paymentIntent.client_secret,
