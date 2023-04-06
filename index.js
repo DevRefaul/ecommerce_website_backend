33ff7577d7fd31333d93e229c7e167b2b997edfb
DevRefaul <devrefaul@gmail.com>
2023-04-06 21:30:31 +0600

    some minor change

--- index.js
+++ index.js
@@ -467,19 +467,19 @@ const dbActions = async () => {
         app.post("/create-payment-intent", async (req, res) => {
             const items = req.body;
 
-            calculateOrderAmount(items);
             // Create a PaymentIntent with the order amount and currency
-            // const paymentIntent = await stripe.paymentIntents.create({
-            //     amount: calculateOrderAmount(items),
-            //     currency: "usd",
-            //     automatic_payment_methods: {
-            //         enabled: true,
-            //     },
-            // });
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: calculateOrderAmount(items),
+                currency: "usd",
+                automatic_payment_methods: {
+                    enabled: true,
+                },
+            });
 
-            // res.send({
-            //     clientSecret: paymentIntent.client_secret,
-            // });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+                paymentIntent
+            });
         });
 
 
